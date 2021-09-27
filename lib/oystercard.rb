fb3d86394f7fa1a4e44c116484312e4cd08f78fa
nejla <nejlalowe@gmail.com>
2021-09-27 22:27:04 +0100

    User Story 2

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,11 @@
 class OysterCard
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(money)
+    self.balance += money
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,8 @@ describe OysterCard do
   it "starts with a balance of 0" do
     expect(subject.balance).to eq 0
   end
+  it "adds money to card" do
+    subject.top_up(2)
+    expect(subject.balance).to eq 2
+  end
 end
