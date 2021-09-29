546260450c80197006b8515be983c976922f50d4
nejla <nejlalowe@gmail.com>
2021-09-29 15:14:19 +0100

    chapter1 to 10,created private method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,46 @@
-class OysterCard
-  attr_accessor :balance
+class Oyster_card
+  MAXIMUM_BALANCE = 90
+  MINIMUM_CHARGE = 1
+  attr_reader :balance
+  attr_accessor :entry_station
+  attr_accessor :exit_station
 
   def initialize
     @balance = 0
+    @in_journey = false
+    @entry_station
+    @exit_station
   end
 
-  def top_up(money)
-    self.balance += money
+  def top_up(amount)
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def touch_in(station)
+    fail "Insufficient funds" if @balance < MINIMUM_CHARGE
+    @in_journey = true
+    @entry_station = station
+    return "In use"
+  end
+
+  def touch_out(station)
+    deduct(MINIMUM_CHARGE)
+    @in_journey = false
+    @exit_station = station
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  # def check_balance
+  #   @balance
+  # end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,81 @@
-require "oystercard"
-describe OysterCard do
-  it "starts with a balance of 0" do
+require "./lib/oystercard.rb"
+
+describe Oyster_card do
+  let(:maximum_balance) {Oyster_card::MAXIMUM_BALANCE}
+  let(:minimum_charge) {Oyster_card::MINIMUM_CHARGE}
+  let(:entry_station) {double :station}
+  let(:exit_station) { double :station }
+  
+  it 'checks the balance is zero at initialisation' do
+    
     expect(subject.balance).to eq 0
   end
-  it "adds money to card" do
-    subject.top_up(2)
-    expect(subject.balance).to eq 2
+
+  describe '.top_up' do
+    it 'tops up account by £10' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it 'raise error if balance exceeds £90' do
+      subject.top_up(maximum_balance)
+      expect {subject.top_up(1)}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
   end
+
+  # it 'deducts £15 from account' do
+  #   subject.top_up(maximum_balance)
+  #   subject.deduct(15)
+  #   expect(subject.balance).to eq 75
+  # end
+
+  describe '.touch_in' do
+    it 'sets card as in use' do
+      subject.top_up(10)
+      expect(subject.touch_in(entry_station)).to eq "In use"
+    end
+
+    it 'checks if card is in_journey?' do
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      expect(subject.in_journey?).to eq true
+    end
+
+    it 'returns error if balance < minimum_charge' do
+      expect {subject.touch_in(entry_station)}.to raise_error "Insufficient funds"
+    end
+  end
+  
+
+  describe '.touch_out' do
+    it 'set card as not in use' do 
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.in_journey?).to eq false
+    end
+
+    it 'reduces balance by min amount £1' do
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      expect {subject.touch_out(exit_station)}.to change{subject.balance}.by(- minimum_charge)
+    end
+  end
+
+  describe 'station' do 
+    it 'stores visited stations' do
+      subject.top_up(10)
+      #s double("St Pancreas", )
+      #station = "St pancreas"
+      subject.touch_in( entry_station)
+      expect(subject.entry_station).to eq entry_station
+    end
+    it 'stores exit station' do
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+  end
+
 end
